5c79860100498a8dc7754dfa62559bace32aa5da
WeiLin <nomorecomputer@gmail.com>
2025-08-11 11:35:49 +0800

    import js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./assets/scss/all.scss";
 // import "bootstrap/dist/js/bootstrap.min.js";
 import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+import "./assets/js/getParameter";
 
 console.log("Hello world---");
